243451b9f26b88a0832afeb69d01417c6c82541b
Luca <gaglia@gmail.com>
2017-02-10 21:31:42 +0100

    Update launch.sh

--- sparker/launch.sh
+++ sparker/launch.sh
@@ -1,5 +1,5 @@
 export SPARK_LOCAL_IP="localhost"
 export SBT_OPTS="-Xmx48g -Xss5g"
-#sbt "run 48 10 /data2/er/data/dbpedia/profiles/dataset1 /data2/er/data/dbpedia/profiles/dataset2 /data2/er/data/dbpedia/groundtruth" > logDbpedia.txt
+sbt "run 48 10 /data2/er/data/dbpedia/profiles/dataset1 /data2/er/data/dbpedia/profiles/dataset2 /data2/er/data/dbpedia/groundtruth" > logDbpedia.txt
 
 
